6f80f9cfcf63b90c810cd2607b43702f76e137e3
Null0x0zero <project1hacker@gmail.com>
2022-01-02 23:41:40 +0300

    Program to check integer if odd/even

---
+++ OddEven.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+int d ;
+printf("Enter an integer to check:\n");
+scanf(" %d",&d);
+
+if ( d % 2 == 0){
+    printf("Integer %d is Even",d);
+}
+else {
+
+    printf("Integer %d is Odd",d);
+} ;
+  
+  return 0;
+
+};
+
+
+
+
