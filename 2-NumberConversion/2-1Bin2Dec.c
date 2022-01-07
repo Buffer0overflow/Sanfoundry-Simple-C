f30420a729282afd37654ca839e9dd45e726e66f
Null0x0zero <project1hacker@gmail.com>
2022-01-07 13:05:49 +0300

    add 1st excerise of group 2 and rearrange folders

---
+++ 2-NumberConversion/2-1Bin2Dec.c
@@ -0,0 +1,30 @@
+/*
+This is a C program to convert binary number to decimal.
+*/
+
+#include <stdio.h>
+int main()
+{
+
+int num , binaryVal , DecValue = 0 ,base = 1 , remainder ;
+
+puts("Enter a binary number(1s and 0s) to convert to Dec:");
+scanf(" %d",&num);
+binaryVal = num;
+
+while ( num > 0){
+
+remainder = num % 10;
+DecValue = DecValue + remainder * base ; 
+num = num /10 ;
+base = base * 2;
+ 
+
+
+}
+printf("You entered %d\n",binaryVal);
+printf("Decimal value of binary is %d",DecValue);
+
+
+ return 0;
+};
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #Sanfoundry-Simple-C
 
 #1. C Examples on Integers
+#2. C Examples on Number Conversion
